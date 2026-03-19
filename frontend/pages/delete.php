eb584a7491a953bb7d2c4eb11f129a6bf027be03
Marcelo <marcelo_mj7@hotmail.com>
2026-03-18 23:45:54 -0300

    att

--- frontend/pages/delete.php
+++ frontend/pages/delete.php
@@ -1,2 +1,19 @@
 <?php
 require_once '../config/database.php';
+
+if (isset($_GET['codigo'])) {
+  $idRecebido = $_GET['codigo'];
+
+  try {
+    $sql = "DELETE FROM metas WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id_recebido);
+
+    if ($stmt->execute()) {
+      header('Location: index.php');
+      exit;
+    }
+  } catch (PDOException $e) {
+    echo "Erro ao desmontar peça: " . $e->getMessage();
+  }
+}
